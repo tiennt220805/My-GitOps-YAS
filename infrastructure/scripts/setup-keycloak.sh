925afea4a76ec986e40ce92f96272671ab6c4b56
Tipie <tiennt220805@gmail.com>
2026-04-19 18:05:51 +0700

    fix script setup keycloak infrastructure

--- infrastructure/scripts/setup-keycloak.sh
+++ infrastructure/scripts/setup-keycloak.sh
@@ -2,13 +2,12 @@
 set -e
 set -x
 
-echo "Installing Keycloak CRDs..."
+echo "Installing Keycloak Operator (Official Bundle)..."
+
 kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
 kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
 
-# BỔ SUNG: Cài đặt Keycloak Operator
-echo "Installing Keycloak Operator..."
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm upgrade --install keycloak-operator bitnami/keycloak-operator \
-  --create-namespace --namespace keycloak-operator
+kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/kubernetes.yml
+
+echo "Waiting for Keycloak Operator to be ready..."
+kubectl rollout status deployment/keycloak-operator -n keycloak-operator --timeout=3m || true
