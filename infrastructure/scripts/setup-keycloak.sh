fca8e773777e20fcec1b32d862604e745b35227b
Tipie <tiennt220805@gmail.com>
2026-04-20 21:25:18 +0700

    move applying RBAC before helm

--- infrastructure/scripts/setup-keycloak.sh
+++ infrastructure/scripts/setup-keycloak.sh
@@ -7,13 +7,13 @@ kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resourc
 kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
 kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/kubernetes.yml
 
+echo "--- Applying RBAC for Keycloak Operator ---"
+kubectl apply -f ../base/keycloak/keycloak/role-binding.yaml
+
 echo "--- Deploying Keycloak Instance ---"
-sleep 30
+sleep 20
 
 helm upgrade --install keycloak ../base/keycloak/keycloak \
   --namespace keycloak --create-namespace \
   -f ../../environments/test/values-shared.yaml \
   -f ./cluster-config.yaml
-
-echo "--- Applying RBAC for Keycloak Operator ---"
-kubectl apply -f ../base/keycloak/keycloak/role-binding.yaml
