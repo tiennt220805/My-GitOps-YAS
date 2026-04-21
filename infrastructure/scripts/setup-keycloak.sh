b43e26135fc9095dd3dab2cdd2c5dbfdf8a68c6a
Tipie <tiennt220805@gmail.com>
2026-04-21 11:19:10 +0700

    fix Keycloak

--- infrastructure/scripts/setup-keycloak.sh
+++ infrastructure/scripts/setup-keycloak.sh
@@ -22,18 +22,6 @@ curl -s https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2
 sed 's/namespace: default/namespace: keycloak-operator/g' | \
 kubectl apply -f - -n keycloak-operator
 
-# Get secret from acid.zalan.do and create secret in postgresql-credentials
-DB_USER=$(kubectl get secret yasadminuser.postgresql.credentials.postgresql.acid.zalan.do -n test-anonymous-cart -o jsonpath='{.data.username}' | base64 -d)
-DB_PASS=$(kubectl get secret yasadminuser.postgresql.credentials.postgresql.acid.zalan.do -n test-anonymous-cart -o jsonpath='{.data.password}' | base64 -d)
-
-kubectl create secret generic postgresql-credentials \
-  -n test-anonymous-cart \
-  --from-literal=username="$DB_USER" \
-  --from-literal=password="$DB_PASS" \
-  --dry-run=client -o yaml | kubectl apply -f -
-
-echo "--- Secret postgresql-credentials has been synchronized ---"
-
 echo "--- Applying RBAC for Keycloak Operator ---"
 kubectl apply -f ../base/keycloak/keycloak/role-binding.yaml
 
